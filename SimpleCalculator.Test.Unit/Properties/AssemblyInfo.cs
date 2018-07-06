b31a5cb800cb243452b728a711348cf8ba36795d
dan.craycraft@lids.com <dan.craycraft@lids.com>
2018-07-06 11:53:15 -0400

    Inintal commit

---
+++ SimpleCalculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lids")]
+[assembly: AssemblyProduct("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright © Lids 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("163895db-a465-48b4-8d3e-788e15e9e4c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,24 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                // In the real world we would want to log this message.
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
